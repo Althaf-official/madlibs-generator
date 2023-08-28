d01b8a7ebff811e0e3c9e875baa1b45c1b0065fb
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-28 18:58:42 +0400

    generated madlibs

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -11,3 +11,20 @@ place = input('Name a place: ')
 adjective = input('Choose an adjective (Describing word): ')
 
 noun3 = input('Choose a noun: ')
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print('Be kind to your', noun, '- footed', p_noun)
+
+print('For a duck may be somebody\'s', noun2, ',')
+
+print('Be kind to your', p_noun, 'in', place)
+
+print('Where the weather is always', adjective, '. \n')
+
+print('You may think that is this the', noun3, ',')
+
+print('Well it is.')
+
+print('------------------------------------------')
